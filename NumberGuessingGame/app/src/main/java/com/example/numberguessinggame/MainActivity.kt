0956e69e19b40e7712b5d8747edf3c93e8c6ac0e
Ploynapat Bunsena <6310610966@student.tu.ac.th>
2023-03-04 20:22:30 +0700

    edit fornt-end

--- NumberGuessingGame/app/src/main/java/com/example/numberguessinggame/MainActivity.kt
+++ NumberGuessingGame/app/src/main/java/com/example/numberguessinggame/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.numberguessinggame
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -15,11 +19,21 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.numberguessinggame.ui.theme.NumberGuessingGameTheme
 import kotlin.random.Random.Default.nextInt
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,13 +44,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             NumberGuessingGameTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(modifier = Modifier.fillMaxSize()) {
                     NumberGuessingGameScreen(randomNumber)
                 }
             }
         }
     }
 }
+//modifier = Modifier.background(MaterialTheme.colors.background)
 
 @Composable
 fun NumberGuessingGameScreen(randomNumber: Int) {
@@ -55,8 +70,17 @@ fun NumberGuessingGameScreen(randomNumber: Int) {
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(R.drawable.baseline_numbers_24),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(60.dp)
+        )
         Text(
             text = stringResource(R.string.app_name),
+            color = MaterialTheme.colors.surface,
+            fontFamily = FontFamily.Monospace,
             fontSize = 24.sp,
             modifier = Modifier.align(Alignment.CenterHorizontally),
             fontWeight = FontWeight.Bold
@@ -65,6 +89,8 @@ fun NumberGuessingGameScreen(randomNumber: Int) {
 
         Text(
             text = stringResource(R.string.header_of_page),
+            color = MaterialTheme.colors.surface,
+            textAlign = TextAlign.Center,
             fontSize = 20.sp,
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
@@ -79,7 +105,9 @@ fun NumberGuessingGameScreen(randomNumber: Int) {
             Text(
                 text = stringResource(R.string.hint, hint),
                 modifier = Modifier.align(Alignment.CenterHorizontally),
+                color = MaterialTheme.colors.surface,
                 fontSize = 20.sp,
+                fontFamily = FontFamily.Monospace,
                 fontWeight = FontWeight.Bold
             )
             Spacer(Modifier.height(16.dp))
@@ -95,8 +123,10 @@ fun NumberGuessingGameScreen(randomNumber: Int) {
                     hint = "Higher"
                 } else {
                     canGuess = false
-                }
-            }) {
+                } },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.White,
+                    contentColor = MaterialTheme.colors.onPrimary)) {
                 Text(stringResource(R.string.guess))
             }
         } else {
@@ -118,10 +148,21 @@ fun NumberGuessingGameScreen(randomNumber: Int) {
         Button(onClick = {
             count = 0
             canGuess = true
-            randomNumber = nextInt(1, 1000)
-        }) {
+            randomNumber = nextInt(1, 1000) },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.onBackground,
+                contentColor = Color.White)
+        ){
+            Icon(
+                imageVector = Icons.Filled.Refresh,
+                contentDescription = "Refresh Icon",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
             Text(stringResource(R.string.restart))
         }
+
+
     }
 }
 
@@ -146,12 +187,26 @@ fun EditNumberField(
     )
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
-fun DefaultPreview() {
+fun LightThemePreview() {
     var randomNumber = nextInt(1, 1001)
 
-    NumberGuessingGameTheme {
+    NumberGuessingGameTheme(darkTheme = false) {
+        NumberGuessingGameScreen(randomNumber)
+    }
+}
+
+/**
+ * Composable that displays what the UI of the app looks like in dark theme in the design tab.
+ */
+@Preview(showBackground = true, backgroundColor = 0x757575)
+@Composable
+fun DarkThemePreview() {
+    var randomNumber = nextInt(1, 1001)
+
+    NumberGuessingGameTheme(darkTheme = true) {
         NumberGuessingGameScreen(randomNumber)
     }
 }
--- NumberGuessingGame/app/src/main/java/com/example/numberguessinggame/ui/theme/Theme.kt
+++ NumberGuessingGame/app/src/main/java/com/example/numberguessinggame/ui/theme/Theme.kt
@@ -5,17 +5,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    background = Gray,
+    onBackground = Black,
+    onPrimary = Black,
+    surface = White,
+//    primaryVariant = Purple700,
+//    secondary = Teal200,
+
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    background = Brown,
+    onBackground = Red,
+    onPrimary = Red,
+    surface = Black,
+//    primaryVariant = Purple700,
+//    secondary = Teal200,
+
 
     /* Other default colors to override
     background = Color.White,
